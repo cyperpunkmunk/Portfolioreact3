9472105c9c77b7c95cb3232c36fa21ad5fbef09a
Alien <kendgibsonjr@gmail.com>
2021-06-13 20:39:23 -0500

    added links in home

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./style.css"
 import logo from "../../../IMG_8450.png"
+import github from "../../../github.png"
+import email from "../../../email.png"
+import linkedin from "../../../linkedin.png"
+import stack from "../../../stackoverflow.png"
 import * as THREE from 'three';
 
 function Home() {
@@ -21,8 +25,21 @@ function Home() {
 
       <div className = "twoIntro">
         
-        <div className = "">
-
+        <div className = "linksHome">
+          <div className = "linkContainer">
+            <a className ="imgLink" href="https://www.linkedin.com/in/kenneth-gibson24/">
+              <img className ="imgLink" src={linkedin} alt="pic"/>
+            </a>
+            <a className ="imgLink" href="https://github.com/cyperpunkmunk">
+              <img className ="imgLink" src={github} alt="pic"/>
+            </a>
+            <a className ="imgLink" href="https://stackoverflow.com/users/14836382/kylast">
+              <img className ="imgLink" src={stack} alt="pic"/>
+            </a>
+            <a className ="imgLink" href="mailto:kendgibsonjr@gmail.com">
+              <img className ="imgLink" src={email} alt="pic"/>
+            </a>
+          </div>
         </div>
 
       </div>

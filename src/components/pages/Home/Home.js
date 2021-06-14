e9516f8b55199ebc262f683359770294eec4e19c
Alien <kendgibsonjr@gmail.com>
2021-06-13 22:49:41 -0500

    added links animations

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./style.css"
 import logo from "../../../IMG_8450.png"
 import github from "../../../github.png"
+import github2 from "../../../github2.png"
+
 import email from "../../../email.png"
+import email2 from "../../../email2.png"
+
 import linkedin from "../../../linkedin.png"
+import linkedin2 from "../../../linkedin2.png"
+
 import stack from "../../../stackoverflow.png"
+import stack2 from "../../../stackoverflow2.png"
+
 import * as THREE from 'three';
 
 function Home() {
@@ -27,17 +35,25 @@ function Home() {
         
         <div className = "linksHome">
           <div className = "linkContainer">
-            <a className ="imgLink" href="https://www.linkedin.com/in/kenneth-gibson24/">
+            
+            <a className ="box" href="https://www.linkedin.com/in/kenneth-gibson24/">
               <img className ="imgLink" src={linkedin} alt="pic"/>
+              <img className ="hover-img" src={linkedin2} alt="pic"/>
             </a>
-            <a className ="imgLink" href="https://github.com/cyperpunkmunk">
+
+            <a className ="box" href="https://github.com/cyperpunkmunk">
               <img className ="imgLink" src={github} alt="pic"/>
+              <img className ="hover-img" src={github2} alt="pic"/>
             </a>
-            <a className ="imgLink" href="https://stackoverflow.com/users/14836382/kylast">
+
+            <a className ="box" href="https://stackoverflow.com/users/14836382/kylast">
               <img className ="imgLink" src={stack} alt="pic"/>
+              <img className ="hover-img" src={stack2} alt="pic"/>
             </a>
-            <a className ="imgLink" href="mailto:kendgibsonjr@gmail.com">
+            
+            <a className ="box" href="mailto:kendgibsonjr@gmail.com">
               <img className ="imgLink" src={email} alt="pic"/>
+              <img className ="hover-img" src={email2} alt="pic"/>
             </a>
           </div>
         </div>
--- src/components/pages/Work/Work.js
+++ src/components/pages/Work/Work.js
@@ -3,12 +3,12 @@ import "./work.css"
 import travel from "../../../hiker-ocean-extreme.jpg"
 function Work() {
   return (
-    <div class = "main">
+    <div className = "main">
       <h1 className = "" >Work</h1>
       
       <div className = "work">
         <div className = "imgContainer">
-            <img className ="" src={travel} alt="pic"/>
+            <img id="travel" src={travel} alt="pic"/>
         </div>
         <p className = "workTitle">travel mentor</p>
       </div>

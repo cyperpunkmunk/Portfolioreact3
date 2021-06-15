ec4d1cee585cc01fdac870606dfb2a6833d7a2b9
Jay bluee <kendgibsonjr@gmail.com>
2021-06-15 09:04:03 -0500

    added work section functionallity and some css

--- src/components/pages/Work/Work.js
+++ src/components/pages/Work/Work.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./work.css"
-import travel from "../../../hiker-ocean-extreme.jpg"
+import travel from "../../../travelMentor.png"
+import star from "../../../stardust.png"
 function Work() {
   return (
-    <div className = "main">
+    <div className = "">
       <h1 className = "" >Work</h1>
       
       <div className = "work">
-        <div className = "imgContainer">
-            <img id="travel" src={travel} alt="pic"/>
+        <div className = "work-container">
+          <img src={travel} alt="pic"/>
+          <div className = "transition-image">
+            <p>
+              We wanted to create an App that made it easy and more accesable for users 
+              to be ale to educate themselves on a new place they have never been to before
+            </p>
+            <a href = "https://dylan-casanova.github.io/Travel-Mentor/">Travel Mentor</a>
+          </div>
         </div>
         <p className = "workTitle">travel mentor</p>
       </div>
